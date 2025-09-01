911da8e8b1a9d5a16c5fc695f099cb5675e62fcc
Navneet Makkad <navneetm0402@gmail.com>
2025-09-01 23:23:07 +0530

    prog2

---
+++ prog2.java
@@ -0,0 +1,27 @@
+//WAP to calculate volume of a box with its width, height and depth
+
+class Box {
+    double width,height,depth;
+    void volume()
+    {
+        System.out.println("Volume is : ");
+        System.out.println(width*height*depth);
+    }
+}
+public class prog2 {
+    public static void main(String[] args) {
+        Box myBox1 = new Box();
+        Box myBox2 =  new Box();
+
+        myBox1.width = 10;
+        myBox1.height = 20;
+        myBox1.depth = 30;
+
+        myBox2.width = 3;
+        myBox2.height = 6;
+        myBox2.depth = 9;
+
+        myBox1.volume();
+        myBox2.volume();
+    }    
+}
